cf83965fd5385c6c42cc04413d031599d02fc048
Julien Schleich <julien.schleich@gmail.com>
2013-10-11 18:45:42 +0200

    check.pl now tar the files for next pipe step

--- check.pl
+++ check.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Archive::Tar;
+use File::Basename;
 
 my ($folderIn, $folderOut, $extension, $toExec) = @ARGV;
-print "Program will check folder $folderIn\n";
 
 do {
 	opendir(DIR, $folderIn) or die $!;
@@ -15,14 +16,24 @@ do {
 		# Verify is the file is not currently using
 		my $res = `lsof | grep $file | wc -l`;
 		if ($res == 0){
+			# Moving file(s?) into $folderOut
 			my $retVal = `mv $folderIn/$file $folderOut`;
+			# Execute whatever 
 			$retVal = `perl $toExec $folderOut/$file`;
+			# Create a tar archive with all the files
+			my $tar = Archive::Tar->new;
+			$tar->add_files("./$folderOut/$file");
+			my ($name,$rep,$ext) = fileparse("$folderOut/$file", qr/\.[^.]*/);
+			$tar->add_files("./$folderOut/${name}.json");
+			$tar->rename("$folderOut/${name}.json", "${name}.json");
+			$tar->rename("$folderOut/${name}.tiff", "${name}.tiff");
+			$tar->write("$folderOut/${name}.tar");
+			# Remove the other files
+			$retVal = `rm -f $folderOut/$file $folderOut/${name}.json`;
 		}
 		else {
 			print "The file $file is currently open, waiting ...\n";
-		}
-		# Execution of the script
-		#my $retVal = `perl $toExec $file`;
+		}		
     }
 	closedir(DIR);
 	sleep 1;
